5a00ab770bcb10774d998dfaa098198b09aa7988
Bhat <shashank.bhat2525@gmail.com>
2022-07-10 22:03:28 +0530

    query param test 2

--- api/internships.js
+++ api/internships.js
@@ -16,28 +16,28 @@ module.exports = (req, res) => {
   let wfh = req.query.wfh;
   res.json(category, location, wfh)
   
-  try {
-    x(`https://internshala.com/internships/`, {
-      items: x(".individual_internship", [
-        {
-          title: ".view_detail_button",
-          link: ".view_detail_button @href",
-          company_name: ".company_name | trim",
-          stipend: ".stipend | trim",
-        },
-      ]),
-    })((error, result) => {
-      let responseJson = new Object();
-      let internshipsArray = new Array();
-      result.items.forEach((item) => {
-        internshipsArray.push(item)
-      })
-      responseJson["total"] = internshipsArray.length
-      responseJson["internhips"] = internshipsArray
-      res.json(responseJson);
-    });
-  } catch (err) {
-    res.json(err);
-  }
+  // try {
+  //   x(`https://internshala.com/internships/`, {
+  //     items: x(".individual_internship", [
+  //       {
+  //         title: ".view_detail_button",
+  //         link: ".view_detail_button @href",
+  //         company_name: ".company_name | trim",
+  //         stipend: ".stipend | trim",
+  //       },
+  //     ]),
+  //   })((error, result) => {
+  //     let responseJson = new Object();
+  //     let internshipsArray = new Array();
+  //     result.items.forEach((item) => {
+  //       internshipsArray.push(item)
+  //     })
+  //     responseJson["total"] = internshipsArray.length
+  //     responseJson["internhips"] = internshipsArray
+  //     res.json(responseJson);
+  //   });
+  // } catch (err) {
+  //   res.json(err);
+  // }
 }
 
